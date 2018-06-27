ce4a3cb2dcd9fa49efeb270fe3a576f6c00c04f7
Mingzhi Zheng <mizheng@microsoft.com>
2018-06-27 23:36:02 +0800

    update build & run script to version 1.4.0

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,3 +1,3 @@
 # example:
 #     nvidia-docker run --rm -it [namespace]/tensorflow:[tag]
-#     nvidia-docker run --rm -it stevezheng23/tensorflow:latest-gpu_cuda8_cudnn6-tensorflow1.7.0-python3.6
+#     nvidia-docker run --rm -it stevezheng23/tensorflow:latest-gpu_cuda8_cudnn6-tensorflow1.4.0-python3.6
