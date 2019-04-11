441779c6d5ce852010bd5db6e313e06282a25d1f
Mingzhi Zheng <mizheng@microsoft.com>
2019-04-11 14:05:13 -0700

    update build & run script for latest docker file

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,3 +1,3 @@
 # example:
-#     nvidia-docker run --rm -it [namespace]/tensorflow:[tag]
-#     nvidia-docker run --rm -it stevezheng23/tensorflow:0.0.3-gpu_cuda9_cudnn7-tensorflow1.12.0-python3.6
+#     nvidia-docker2 run --rm -it [namespace]/tensorflow:[tag]
+#     nvidia-docker2 run --rm -it stevezheng23/tensorflow:0.0.4-gpu_cuda10_cudnn7-tensorflow1.13.1-python3.6
