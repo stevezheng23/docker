e1ee5f6e2aad7dffdb08955ed3aa43f20e48ce19
Mingzhi Zheng <mizheng@microsoft.com>
2019-04-13 23:23:57 -0700

    update build/run example scripts for pytorch/tensorflow docker

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -1,3 +1,3 @@
-# example:
+# current version: 0.0.2-gpu_cuda8_cudnn6-torch0.3.1-python3.6
 #     nvidia-docker run --rm -it [namespace]/pytorch:[tag]
-#     nvidia-docker run --rm -it stevezheng23/pytorch:0.0.2-gpu_cuda8_cudnn6-torch0.3.1-python3.6
+#     nvidia-docker run --rm -it stevezheng23/pytorch:latest
--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,3 +1,3 @@
-# example:
+# current version: 0.0.4-gpu_cuda10_cudnn7-tensorflow1.13.1-python3.6
 #     nvidia-docker2 run --rm -it [namespace]/tensorflow:[tag]
-#     nvidia-docker2 run --rm -it stevezheng23/tensorflow:0.0.4-gpu_cuda10_cudnn7-tensorflow1.13.1-python3.6
+#     nvidia-docker2 run --rm -it stevezheng23/tensorflow:latest
